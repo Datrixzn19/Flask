a7e51b102539fabe3d60bed8245b587ea12af304
David19 <davidleonel911@gmail.com>
2025-08-06 20:59:41 -0500

    creacion de rutas

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+    return render_template("auth/register.html")
+
+@app.route("/prices")
+def prices():
+    return render_template("prices.html")
