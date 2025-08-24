38d075ac9de6e92f368e8ca502d75d700e65bdd7
David19 <davidleonel911@gmail.com>
2025-08-24 12:02:31 -0500

    base de la pagina web con rutas princiapales y estilos basicso

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-
-@app.route("/")#esta es la ruta base 
-def index():
-    return "Esta es la pagina principal "
 
 @app.route("/usuario/<name>")#ruta personalizada
 def saludo(name):
     return f"Bienvenido, {name}"
+
+#Creando Rutas 
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():   
+    return render_template("about.html")      
+@app.route("/register")
+def register():   
+    return render_template("auth/register.html")      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
