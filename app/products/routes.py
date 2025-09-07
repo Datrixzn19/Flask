a7baef5f2223f3844892f257d7c944887d6418e0
David19 <davidleonel911@gmail.com>
2025-09-07 09:13:02 -0500

    .

--- app/products/routes.py
+++ app/products/routes.py
@@ -4,7 +4,7 @@ import os, json
 
 products_bp = Blueprint("products", __name__)
 
-# ------------------ RUTA PRINCIPAL ------------------
+# Index
 @products_bp.route("/", methods=["GET", "POST"])
 def index():
     db = get_db(current_app)
@@ -14,7 +14,7 @@ def index():
         cantidad = request.form["cantidad"]
         precio = request.form["precio"]
 
-        # --- Guardar en SQLite ---
+        # Guardar sqlite
         cursor = db.execute(
             "INSERT INTO products (nombre, cantidad, precio) VALUES (?, ?, ?)",
             (nombre, cantidad, precio)
@@ -22,7 +22,7 @@ def index():
         db.commit()
         producto_id = cursor.lastrowid  # ID autoincremental
 
-        # --- Guardar también en datos.json ---
+        # Guardar en json 
         datos_dir = os.path.join(current_app.root_path, "..", "datos")
         os.makedirs(datos_dir, exist_ok=True)
         json_path = os.path.join(datos_dir, "datos.json")
@@ -47,11 +47,11 @@ def index():
 
         return redirect(url_for("products.index"))
 
-    # --- Mostrar productos ---
+    # Mostrar productos
     productos = db.execute("SELECT * FROM products").fetchall()
     return render_template("inventario/inventario.html", productos=productos)
 
-# ------------------ EDITAR PRODUCTO ------------------
+# EDITAR 
 @products_bp.route("/editar/<int:id>", methods=["POST"])
 def editar(id):
     db = get_db(current_app)
@@ -59,14 +59,14 @@ def editar(id):
     cantidad = request.form["cantidad"]
     precio = request.form["precio"]
 
-    # --- Actualizar en SQLite ---
+    # actualizar en sqlite
     db.execute(
         "UPDATE products SET nombre=?, cantidad=?, precio=? WHERE id=?",
         (nombre, cantidad, precio, id)
     )
     db.commit()
 
-    # --- Actualizar en datos.json ---
+    #  Actualizar el json
     datos_dir = os.path.join(current_app.root_path, "..", "datos")
     json_path = os.path.join(datos_dir, "datos.json")
 
@@ -85,14 +85,14 @@ def editar(id):
 
     return redirect(url_for("products.index"))
 
-# ------------------ ELIMINAR PRODUCTO ------------------
+# ELIMINAR
 @products_bp.route("/eliminar/<int:id>", methods=["POST"])
 def eliminar(id):
     db = get_db(current_app)
     db.execute("DELETE FROM products WHERE id=?", (id,))
     db.commit()
 
-    # --- Eliminar del JSON ---
+    #Eliminar del JSON
     datos_dir = os.path.join(current_app.root_path, "..", "datos")
     json_path = os.path.join(datos_dir, "datos.json")
 
