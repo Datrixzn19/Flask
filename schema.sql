4cbd73c47cdb6d2cd48195c67daf4d47841e8162
David19 <davidleonel911@gmail.com>
2025-09-24 09:42:34 -0500

    creacion de tablas categorias y productos para el CRUD

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 -- proyecto_desarrollo_web.usuarios definition
 
+--TABLA usuarios 
 CREATE TABLE `usuarios` (
   `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
@@ -7,3 +8,24 @@ CREATE TABLE `usuarios` (
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+  --CRUD CON MYSQL 
+-- proyecto_desarrollo_web.productos definition
+CREATE TABLE `productos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `categoria_id` int NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `stock` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_categoria` (`categoria_id`),
+  CONSTRAINT `fk_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+-- proyecto_desarrollo_web.categorias definition
+
+CREATE TABLE `categorias` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(15) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
