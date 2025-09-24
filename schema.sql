268cda6f89a1e8a5dace1437ad1deb91afffc40a
David19 <davidleonel911@gmail.com>
2025-09-24 09:43:30 -0500

    tablas

--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
--- proyecto_desarrollo_web.usuarios definition
-
---TABLA usuarios 
+      --BASE DE DATOS MYSQL - DBEAVER 
+-- proyecto_desarrollo_web.usuarios definition 
 CREATE TABLE `usuarios` (
   `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
