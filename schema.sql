53abde50b8376efc628ebfab6eae4a08d8efb66b
David19 <davidleonel911@gmail.com>
2025-09-27 11:52:23 -0500

    creacion archivos html para el crud

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
       --BASE DE DATOS MYSQL - DBEAVER 
+      
 -- proyecto_desarrollo_web.usuarios definition 
 CREATE TABLE `usuarios` (
   `nombre` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
